e93ee83775f90663a04adffee8d5410736086c3d
kiko <A7zure@gmail.com>
2025-11-16 17:42:56 -0700

    Initial Commit

---
+++ little cheese/little_cheeseApp.swift
@@ -0,0 +1,17 @@
+//
+//  little_cheeseApp.swift
+//  little cheese
+//
+//  Created by jdjdind dhdjkd on 2025-11-16.
+//
+
+import SwiftUI
+
+@main
+struct little_cheeseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
